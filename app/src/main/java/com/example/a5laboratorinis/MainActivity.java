f3eaef261bc340fb99d5778b4a40b766b9687b0e
MrMariu5 <marius.sadreika@gmail.com>
2020-12-12 11:40:56 +0200

    Base

---
+++ app/src/main/java/com/example/a5laboratorinis/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.a5laboratorinis;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
